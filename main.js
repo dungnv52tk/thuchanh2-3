c92e8bea7f49141aec9516ca6af62087d18fd32d
dungnv52tk <dungnv52tk@gmail.com>
2019-03-26 15:10:51 +0700

    first commit

---
+++ main.js
@@ -0,0 +1,8 @@
+let a = prompt("Nhap vao so thu nhat: ");
+let b = prompt("Nhap vao so thu hai: ");
+let c = a % b;
+if (c == 0) {
+    alert("a la boi so cua b")
+} else {
+    alert("a khong phai la boi so cua b")
+}
